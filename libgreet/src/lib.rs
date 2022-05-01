10fa7dd29445d2687c29683d71d24f9d3c53ef64
Julius Liu <juliusl@microsoft.com>
2022-04-30 18:28:32 -0700

    Feat. Added example fn that returns a string

--- libgreet/src/lib.rs
+++ libgreet/src/lib.rs
@@ -1,8 +1,29 @@
-use std::{ffi::CStr, os::raw::c_char};
+use std::{
+    ffi::{CStr, CString},
+    os::raw::c_char,
+};
 
 // new!
 #[no_mangle]
 pub unsafe extern "C" fn greet(name: *const c_char) {
     let cstr = CStr::from_ptr(name);
-    println!("Vodka vodka whiskey, {}!", cstr.to_str().unwrap());
+    println!("Greetings, {}!", cstr.to_str().unwrap());
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn greet_format(name: *const c_char) -> *mut c_char {
+    let cstr = CStr::from_ptr(name);
+
+    if let Ok(formatted) = cstr.to_str() {
+        let formatted = format!("formatting {}", formatted);
+
+        let val = match CString::new(formatted.as_str()) {
+            Ok(v) => v,
+            Err(_) => CString::default(),
+        };
+
+        val.into_raw()
+    } else {
+        CString::default().into_raw()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use cstr::cstr;
 use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
-use std::{os::raw::c_char, sync::mpsc::channel, time::Duration, path::PathBuf};
+use std::{os::raw::c_char, sync::mpsc::channel, time::Duration, path::PathBuf, ffi::{CStr, CString}};
 
 use libloading::{Library, Symbol};
 
@@ -19,12 +19,19 @@ fn main() {
     loop {
         match rx.recv() {
             Ok(event) => {
-                if let DebouncedEvent::Write(path) = event {
-                    if path.ends_with("libgreet.so") {
+                match event {
+                    DebouncedEvent::Write(path) => if path.ends_with("libgreet.so") {
                         if let Err(e) = load_and_print(&path) {
                             eprintln!("error: {}", e);
                         }
-                    }
+                    },
+                    // TODO: Not sure how to handle MacOS/Apple Silicon yet 
+                    DebouncedEvent::Create(path) => if path.ends_with("libgreet.dylib") {
+                        if let Err(e) = load_and_print(&path) {
+                            eprintln!("error: {}", e);
+                        }
+                    },
+                    _ => continue, 
                 }
             }
             Err(e) => println!("watch error: {:?}", e),
@@ -36,7 +43,16 @@ fn load_and_print(libpath: &PathBuf) -> Result<(), libloading::Error> {
     unsafe {
         let lib = Library::new(libpath)?;
         let greet: Symbol<unsafe extern "C" fn(name: *const c_char)> = lib.get(b"greet")?;
+        let format: Symbol<unsafe extern "C" fn(name: *const c_char) -> *mut c_char> = lib.get(b"greet_format")?; 
         greet(cstr!("reloading").as_ptr());
+
+        let output = format(cstr!(formatting).as_ptr());
+
+        // Since we're getting a *mut c_char from this function, we need to 
+        // use CString::from_raw to handle the ptr safely
+        let output = CString::from_raw(output);
+
+        println!("Result: {}", output.to_str().unwrap());
 
         Ok(())
     }
